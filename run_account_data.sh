c2329a37927cc9bc25fe8308db95807b6e4130e1
root <root@rippled-001.xrptipbot.com>
2018-02-27 02:43:13 +0100

    fix do

--- run_account_data.sh
+++ run_account_data.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "Starting in 1 minute..."
-sleep 60; cd /ledger/; while true; node account_data.js 2>&1 > account_data.log && echo $(date)" Done. Restarting (600)..." && sleep 600; done
+
+sleep 60
+cd /ledger/ 
+
+while true; do 
+  node account_data.js 2>&1 > account_data.log && \
+  echo $(date)" Done. Restarting (600)..." && \
+  sleep 600;
+done
